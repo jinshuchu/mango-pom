0a57235cc48f532d09f46561a48c11a792dc545d
KSMASTERS <ksmasterasy@icloud.com>
2019-06-26 23:49:36 +0800

    增加登陆接口，验证用户权限信息

--- mango-admin/src/main/java/com/louis/mango/admin/controller/SysLoginController.java
+++ mango-admin/src/main/java/com/louis/mango/admin/controller/SysLoginController.java
@@ -2,9 +2,21 @@ package com.louis.mango.admin.controller;
 
 import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
+import com.louis.mango.admin.VO.LoginBean;
+import com.louis.mango.admin.model.SysUser;
+import com.louis.mango.admin.security.JwtAuthenticatioToken;
+import com.louis.mango.admin.service.SysLoginLogService;
+import com.louis.mango.admin.service.SysUserService;
+import com.louis.mango.admin.util.IPUtils;
+import com.louis.mango.admin.util.PasswordUtils;
+import com.louis.mango.admin.util.SecurityUtils;
 import com.louis.mango.common.utils.IOUtils;
+import com.louis.mango.core.http.HttpResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
@@ -24,6 +36,12 @@ public class SysLoginController {
 
     @Autowired
     private Producer producer;
+    @Autowired
+    private SysUserService sysUserService;
+    @Autowired
+    private SysLoginLogService sysLoginLogService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @GetMapping("captcha.jpg")
     public void captcha(HttpServletResponse response, HttpServletRequest request) throws ServletException, IOException {
@@ -40,5 +58,39 @@ public class SysLoginController {
         ServletOutputStream out = response.getOutputStream();
         ImageIO.write(image, "jpg", out);
         IOUtils.closeQuietly(out);
+    }
+
+
+    @PostMapping(value = "/login")
+    public HttpResult login(@RequestBody LoginBean loginBean, HttpServletRequest request) throws IOException {
+        String username = loginBean.getAccount();
+        String password = loginBean.getPassword();
+        String captcha = loginBean.getCaptcha();
+        // 从session中获取之前保存的验证码跟前台传来的验证码进行匹配
+        Object kaptcha = request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY);
+//		if(kaptcha == null){
+//			return HttpResult.error("验证码已失效");
+//		}
+//		if(!captcha.equals(kaptcha)){
+//			return HttpResult.error("验证码不正确");
+//		}
+        // 用户信息
+        SysUser user = sysUserService.findByName(username);
+        // 账号不存在、密码错误
+        if (user == null) {
+            return HttpResult.error("账号不存在");
+        }
+        if (!PasswordUtils.matches(user.getSalt(), password, user.getPassword())) {
+            return HttpResult.error("密码不正确");
+        }
+        // 账号锁定
+        if (user.getStatus() == 0) {
+            return HttpResult.error("账号已被锁定,请联系管理员");
+        }
+        // 系统登录认证
+        JwtAuthenticatioToken token = SecurityUtils.login(request, username, password, authenticationManager);
+        // 记录登录日志
+        sysLoginLogService.writeLoginLog(username, IPUtils.getIpAddr(request));
+        return HttpResult.ok(token);
     }
 }
--- mango-admin/src/main/java/com/louis/mango/admin/service/impl/SysUserServiceImpl.java
+++ mango-admin/src/main/java/com/louis/mango/admin/service/impl/SysUserServiceImpl.java
@@ -1,79 +1,177 @@
 package com.louis.mango.admin.service.impl;
 
-import com.louis.mango.admin.dao.SysUserMapper;
-import com.louis.mango.admin.model.SysUser;
-import com.louis.mango.admin.model.SysUserRole;
-import com.louis.mango.admin.service.SysUserService;
-import com.louis.mango.common.utils.DateTimeUtils;
-import com.louis.mango.common.utils.PoiUtils;
-import com.louis.mango.core.page.MybatisPageHelper;
-import com.louis.mango.core.page.PageRequest;
-import com.louis.mango.core.page.PageResult;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import com.louis.mango.admin.dao.SysRoleMapper;
+import com.louis.mango.admin.dao.SysUserMapper;
+import com.louis.mango.admin.dao.SysUserRoleMapper;
+import com.louis.mango.admin.model.SysMenu;
+import com.louis.mango.admin.model.SysRole;
+import com.louis.mango.admin.model.SysUser;
+import com.louis.mango.admin.model.SysUserRole;
+import com.louis.mango.admin.service.SysMenuService;
+import com.louis.mango.admin.service.SysUserService;
+import com.louis.mango.common.utils.DateTimeUtils;
+import com.louis.mango.common.utils.PoiUtils;
+import com.louis.mango.core.page.MybatisPageHelper;
+import com.louis.mango.core.page.PageRequest;
+import com.louis.mango.core.page.PageResult;
 
 @Service
 public class SysUserServiceImpl  implements SysUserService {
 
 	@Autowired
 	private SysUserMapper sysUserMapper;
+	@Autowired
+	private SysMenuService sysMenuService;
+	@Autowired
+	private SysUserRoleMapper sysUserRoleMapper;
+	@Autowired
+	private SysRoleMapper sysRoleMapper;
 
-
+	@Transactional
 	@Override
 	public int save(SysUser record) {
-		return 0;
+		Long id = null;
+		if(record.getId() == null || record.getId() == 0) {
+			// 新增用户
+			sysUserMapper.insertSelective(record);
+			id = record.getId();
+		} else {
+			// 更新用户信息
+			sysUserMapper.updateByPrimaryKeySelective(record);
+		}
+		// 更新用户角色
+		if(id != null && id == 0) {
+			return 1;
+		}
+		if(id != null) {
+			for(SysUserRole sysUserRole:record.getUserRoles()) {
+				sysUserRole.setUserId(id);
+			}
+		} else {
+			if(!record.getUserRoles().isEmpty()) {
+				sysUserRoleMapper.deleteByUserId(record.getId());
+			}
+		}
+		for(SysUserRole sysUserRole:record.getUserRoles()) {
+			sysUserRoleMapper.insertSelective(sysUserRole);
+		}
+		return 1;
 	}
 
 	@Override
 	public int delete(SysUser record) {
-		return 0;
+		return sysUserMapper.deleteByPrimaryKey(record.getId());
 	}
 
 	@Override
 	public int delete(List<SysUser> records) {
-		return 0;
+		for(SysUser record:records) {
+			delete(record);
+		}
+		return 1;
 	}
 
 	@Override
 	public SysUser findById(Long id) {
-		return null;
+		return sysUserMapper.selectByPrimaryKey(id);
 	}
-
+	
+	@Override
+	public SysUser findByName(String name) {
+		SysUser sysUser = sysUserMapper.findByName(name);
+		List<SysUserRole> userRoles = findUserRoles(sysUser.getId());
+		sysUser.setUserRoles(userRoles);
+		sysUser.setRoleNames(getRoleNames(userRoles));
+		return sysUser;
+	}
+	
 	@Override
 	public PageResult findPage(PageRequest pageRequest) {
-		return MybatisPageHelper.findPage(pageRequest, sysUserMapper);
+		PageResult pageResult = null;
+		Object name = pageRequest.getParamValue("name");
+		Object email = pageRequest.getParamValue("email");
+		if(name != null) {
+			if(email != null) {
+				pageResult = MybatisPageHelper.findPage(pageRequest, sysUserMapper, "findPageByNameAndEmail", name, email);
+			} else {
+				pageResult = MybatisPageHelper.findPage(pageRequest, sysUserMapper, "findPageByName", name);
+			}
+		} else {
+			pageResult = MybatisPageHelper.findPage(pageRequest, sysUserMapper);
+		}
+		// 加载用户角色信息
+		findUserRoles(pageResult);
+		return pageResult;
+	}
+	
+	/**
+	 * 加载用户角色
+	 * @param pageResult
+	 */
+	private void findUserRoles(PageResult pageResult) {
+		List<?> content = pageResult.getContent();
+		for(Object object:content) {
+			SysUser sysUser = (SysUser) object;
+			List<SysUserRole> userRoles = findUserRoles(sysUser.getId());
+			sysUser.setUserRoles(userRoles);
+			sysUser.setRoleNames(getRoleNames(userRoles));
+		}
+	}
+
+	private String getRoleNames(List<SysUserRole> userRoles) {
+		StringBuilder sb = new StringBuilder();
+		for(Iterator<SysUserRole> iter=userRoles.iterator(); iter.hasNext();) {
+			SysUserRole userRole = iter.next();
+			SysRole sysRole = sysRoleMapper.selectByPrimaryKey(userRole.getRoleId());
+			if(sysRole == null) {
+				continue ;
+			}
+			sb.append(sysRole.getRemark());
+			if(iter.hasNext()) {
+				sb.append(", ");
+			}
+		}
+		return sb.toString();
 	}
 
 	@Override
-	public SysUser findByName(String username) {
-		return null;
-	}
-
-	@Override
-	public Set<String> findPermissions(String userName) {
-		return null;
+	public Set<String> findPermissions(String userName) {	
+		Set<String> perms = new HashSet<>();
+		List<SysMenu> sysMenus = sysMenuService.findByUser(userName);
+		for(SysMenu sysMenu:sysMenus) {
+			if(sysMenu.getPerms() != null && !"".equals(sysMenu.getPerms())) {
+				perms.add(sysMenu.getPerms());
+			}
+		}
+		return perms;
 	}
 
 	@Override
 	public List<SysUserRole> findUserRoles(Long userId) {
-		return null;
+		return sysUserRoleMapper.findUserRoles(userId);
 	}
-
+	
 	@Override
 	public File createUserExcelFile(PageRequest pageRequest) {
 		PageResult pageResult = findPage(pageRequest);
 		return createUserExcelFile(pageResult.getContent());
 	}
-
+	
 	public static File createUserExcelFile(List<?> records) {
 		if (records == null) {
 			records = new ArrayList<>();
@@ -119,4 +217,5 @@ public class SysUserServiceImpl  implements SysUserService {
 		}
 		return PoiUtils.createExcelFile(workbook, "download_user");
 	}
+
 }
