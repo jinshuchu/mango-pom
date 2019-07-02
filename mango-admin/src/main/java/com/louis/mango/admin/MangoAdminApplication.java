363622a2545af20778854f1cc284b8a54fd437a8
KSMASTERS <ksmasterasy@icloud.com>
2019-07-02 09:51:13 +0800

    整合docker

--- mango-admin/src/main/java/com/louis/mango/admin/MangoAdminApplication.java
+++ mango-admin/src/main/java/com/louis/mango/admin/MangoAdminApplication.java
@@ -2,7 +2,9 @@ package com.louis.mango.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.louis.mango.admin"})  // 可以不配置，因为当前启动类默认会扫描当前启动累包及其子类
 public class MangoAdminApplication {
 
--- mongo-backup/src/main/java/com/louis/mango/backup/MangoBackupApplication.java
+++ mongo-backup/src/main/java/com/louis/mango/backup/MangoBackupApplication.java
@@ -2,17 +2,15 @@ package com.louis.mango.backup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
-<<<<<<< HEAD
-=======
 /**
  * 启动器
  * @author Louis
  * @date Jan 15, 2019
  */
->>>>>>> 13dbe0d8b2ddd1fc19ee76cd352c8c23e43f3787
 @SpringBootApplication(scanBasePackages={"com.louis.mango"})
+@EnableDiscoveryClient
 public class MangoBackupApplication {
 
 	public static void main(String[] args) {
